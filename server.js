ea6d37c88bfc9bd227b6ddafef6b6fc9cb9feaa4
julianamtyler <julianam.tyler@gmail.com>
2019-01-20 20:35:38 -0500

    made changes to server file

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 
@@ -10,12 +9,9 @@ app.use(express.urlencoded({extended: true }))
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://socket:12345s@ds159993.mlab.com:59993/heroku_tnvm1j95', 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://todo:12345a@ds159993.mlab.com:59993/heroku_tnvm1j95', 
 { useNewUrlParser: true });
 
-// mongoose.connect('mongodb://localhost/todoListSocket',{useNewUrlParser: true});
-
-require('./sockets/todo-sockets')(io);
 require('./routes/api-routes')(app);
 require('./routes/html-routes')(app);
 server.listen(PORT, ()=>{
